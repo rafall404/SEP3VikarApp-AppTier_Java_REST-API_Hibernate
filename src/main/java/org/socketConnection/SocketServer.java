713fa0ccd594110b2562653f2d6b5b397b8d1a37
chukwukodinaka <285152@via.dk>
2020-06-02 19:29:44 +0200

    " more Rafactoring of the Project structure"

--- src/main/java/org/socketConnection/SocketServer.java
+++ src/main/java/org/socketConnection/SocketServer.java
@@ -1,6 +1,8 @@
 package org.socketConnection;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,12 +12,25 @@ public class SocketServer {
         try{
             ServerSocket welcomeSocket = new ServerSocket(9898);
 
-            while(true){
-                Socket socket = welcomeSocket.accept();
-                System.out.println("Presentation Tier connected ######");
-            }
 
-        }catch(IOException e){
+                Socket socket = welcomeSocket.accept();
+                ObjectInputStream inFromClient = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream outToClient = new ObjectOutputStream(socket.getOutputStream());
+
+                outToClient.writeObject("You have been connected to the App tier... You may now begin to Consume RestFul Web Services... Congratulations!!!");
+                String connection = (String) inFromClient.readObject();
+                if(!connection.equals(null))
+                {
+                    System.out.println("Presentation Tier has been Connected");
+                    System.out.println("Terminated Connection");
+                }
+
+                inFromClient.close();
+                outToClient.close();
+                socket.close();
+
+
+        }catch(IOException | ClassNotFoundException e){
             e.printStackTrace();
         }
     }
